be72fdf9017495368f1412b182ca9c99633093db
Mustu19 <codewithmustu@gmail.com>
2024-04-07 01:34:15 +0530

    Redux-api,features,store for User Management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,22 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
+// import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>      
+    <Route path="/" element={<App />}>
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-      <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -10,8 +10,15 @@ import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../../redux/api/usersApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
+
 
 const Navigation = () => {
+
+    const { userInfo } = useSelector((state) => state.auth);
+    // const { cartItems } = useSelector((state) => state.cart);
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showSidebar, setShowSidebar] = useState(false);
@@ -20,6 +27,20 @@ const Navigation = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation()
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/login");
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
         <div
             style={{ zIndex: 9999 }}
@@ -60,6 +81,20 @@ const Navigation = () => {
                     </div>
                 </Link>
             </div>
+
+            <div className="relative">
+                <button
+                    onClick={toggleDropdown}
+                    className="flex items-center text-gray-800 focus:outline-none"
+                >
+                    {userInfo ? (
+                        <span className="text-white">{userInfo.username}</span>
+                    ) : (
+                        <></>
+                    )}
+                </button>
+            </div>
+
             <ul>
                 <li>
                     <Link
